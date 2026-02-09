efde66bed44c03e5b1ef6dc49efe1b0e9b2a5384
Yorick Nap <144012023+codeyorick@users.noreply.github.com>
2026-02-09 20:04:28 +0100

    Add uuid param matcher

---
+++ src/params/uuid.ts
@@ -0,0 +1,13 @@
+import type { ParamMatcher } from "@sveltejs/kit"
+import * as z from 'zod'
+
+const uuidSchema = z.uuid();
+
+export const match = ((param: string) => {
+  try {
+    uuidSchema.parse(param)
+    return true
+  } catch {
+    return false
+  }
+}) as ParamMatcher
