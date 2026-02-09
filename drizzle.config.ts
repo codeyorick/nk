87a7bab15a7239c5902c587c0597496a113d1f57
Yorick Nap <144012023+codeyorick@users.noreply.github.com>
2026-02-09 19:00:57 +0100

    Fix database URL and format `drizzle.config.ts`

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ if (!process.env.DATABASE_DB) throw new Error("DATABASE_DB is not set")
 if (!process.env.DATABASE_HOST) throw new Error("DATABASE_HOST is not set")
 if (!process.env.DATABASE_PORT) throw new Error("DATABASE_PORT is not set")
 
-const databaseUrl = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_DB}`;
+const databaseUrl = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_DB}`
 
 export default defineConfig({
   schema: "./src/lib/server/db/schema.ts",
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,16 @@
 import { drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
-import * as schema from "./schema"
 import { env } from "$env/dynamic/private"
+import * as schema from "./schema"
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set")
+if (!env.DATABASE_USER) throw new Error("DATABASE_USER is not set")
+if (!env.DATABASE_PASSWORD) throw new Error("DATABASE_PASSWORD is not set")
+if (!env.DATABASE_DB) throw new Error("DATABASE_DB is not set")
+if (!env.DATABASE_HOST) throw new Error("DATABASE_HOST is not set")
+if (!env.DATABASE_PORT) throw new Error("DATABASE_PORT is not set")
 
-const client = postgres(env.DATABASE_URL)
+const databaseUrl = `postgres://${env.DATABASE_USER}:${env.DATABASE_PASSWORD}@${env.DATABASE_HOST}:${env.DATABASE_PORT}/${env.DATABASE_DB}`
+
+const client = postgres(databaseUrl)
 
 export const db = drizzle(client, { schema })
