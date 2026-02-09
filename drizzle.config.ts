75311bfa3b131d13cec23f9f577dd36d3c601206
Yorick Nap <144012023+codeyorick@users.noreply.github.com>
2026-02-09 18:55:07 +0100

    Split database url into parts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "drizzle-kit"
 
-if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set")
+if (!process.env.DATABASE_USER) throw new Error("DATABASE_USER is not set")
+if (!process.env.DATABASE_PASSWORD) throw new Error("DATABASE_PASSWORD is not set")
+if (!process.env.DATABASE_DB) throw new Error("DATABASE_DB is not set")
+if (!process.env.DATABASE_HOST) throw new Error("DATABASE_HOST is not set")
+if (!process.env.DATABASE_PORT) throw new Error("DATABASE_PORT is not set")
+
+const databaseUrl = `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_DB}`;
 
 export default defineConfig({
   schema: "./src/lib/server/db/schema.ts",
   dialect: "postgresql",
-  dbCredentials: { url: process.env.DATABASE_URL },
+  dbCredentials: { url: databaseUrl },
   verbose: true,
   strict: true
 })
