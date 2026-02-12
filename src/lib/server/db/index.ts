125eeea9005cf7702d707efaf5aa6a33aacf9135
Yorick Nap <144012023+codeyorick@users.noreply.github.com>
2026-02-12 21:21:53 +0100

    Fix relations import in database initialization

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js"
 import { env } from "$env/dynamic/private"
 import * as schema from "./schema"
-import { relations } from "./schema"
 
 if (!env.DATABASE_USER) throw new Error("DATABASE_USER is not set")
 if (!env.DATABASE_PASSWORD) throw new Error("DATABASE_PASSWORD is not set")
@@ -11,4 +10,4 @@ if (!env.DATABASE_PORT) throw new Error("DATABASE_PORT is not set")
 
 const databaseUrl = `postgres://${env.DATABASE_USER}:${env.DATABASE_PASSWORD}@${env.DATABASE_HOST}:${env.DATABASE_PORT}/${env.DATABASE_DB}`
 
-export const db = drizzle(databaseUrl, { schema, relations, casing: "snake_case" })
+export const db = drizzle(databaseUrl, { schema, relations: schema.relations, casing: "snake_case" })
